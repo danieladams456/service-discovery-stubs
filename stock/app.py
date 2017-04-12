f0440fcce640e703a3458c1d3c2b432a7e0930a3
Daniel Adams <danieladams456@gmail.com>
2017-04-12 06:39:05 -0400

    EXTRABACON

--- stock/app.py
+++ stock/app.py
@@ -1,6 +1,17 @@
+import os
 import random
+import requests
+
 from flask import Flask
 app = Flask(__name__)
+
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+#BASE_URL = "http://services.internal.dadams.io"
+BASE_URL = os.environ['BASE_URL']
 
 @app.route("/")
 def main():
@@ -8,11 +19,23 @@ def main():
 
 @app.route("/stock/<symbol>")
 def stock(symbol):
-    return "Stock report for " + symbol + ": " + random_report()
+    message =  "Stock report for " + symbol + ": " + random_report()
+    if symbol == "extrabacon":
+        message += "<br>" + extra_bacon(symbol)
+    return message
 
 def random_report():
     possibilities = ["Good", "Bad", "Ugly"]
     return random.choice(possibilities)
+
+#get a stock too to showcase service discovery
+def extra_bacon(city):
+    url = BASE_URL + "/weather/" + city
+    logger.info("EXTRABACON Service URL: " + url)
+    r = requests.get(url)
+    message = "Querying EXTRABACON URL " + url + "<br>" + r.text
+    return message
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- weather/app.py
+++ weather/app.py
@@ -1,6 +1,17 @@
+import os
 import random
+import requests
+
 from flask import Flask
 app = Flask(__name__)
+
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+#BASE_URL = "http://services.internal.dadams.io"
+BASE_URL = os.environ['BASE_URL']
 
 @app.route("/")
 def main():
@@ -8,7 +19,10 @@ def main():
 
 @app.route("/weather/<city>")
 def weather(city):
-    return "Weather in " + city + ": " + random_weather()
+    message =  "Weather in " + city + ": " + random_weather()
+    if city == "extrabacon":
+        message += "<br>" + extra_bacon(city)
+    return message
 
 def random_weather():
     possibilities = ["Sunny and hot",
@@ -16,6 +30,15 @@ def random_weather():
                      "Not sunny but hot",
                      "Not sunny and not hot"]
     return random.choice(possibilities)
+
+#get a stock too to showcase service discovery
+def extra_bacon(symbol):
+    url = BASE_URL + "/stock/" + symbol
+    logger.info("EXTRABACON Service URL: " + url)
+    r = requests.get(url)
+    message = "Querying EXTRABACON URL " + url + "<br>" + r.text
+    return message
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
